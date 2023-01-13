bc60cf1a6f84a78bf7fb7eff7ea43516b1f6d510
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-13 11:24:46 -0500

    update files

---
+++ main.js
@@ -0,0 +1,32 @@
+(function () {
+    "use strict";
+// ex) import{ name, last } from "./name.js";
+// export will be on the other files ex. export const name = "Amber";
+// export ex. function greeting() {
+    /*console.log("Hi");
+}
+export default greeting; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
