738217172f639787b96dd8e62b60b7e9bf025c81
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-25 01:26:39 -0500

    update/testing

--- game.js
+++ game.js
@@ -38,59 +38,58 @@ Game.prototype.deal = function () {
 
 ////// Draw Cards(place into game depo) //////
 Game.prototype.draw = function () {
-  const player1Card = this.player1.hand.shift();
-  const player2Card = this.player2.hand.shift();
-  this.depo = [...this.depo, player1Card, player2Card];
+  const player1Card = this.player1.hand.shift(0);
+  const player2Card = this.player2.hand.shift(0);
+  this.depo = [player1Card, player2Card];
 };
 
 ////// Compare Cards //////
 Game.prototype.compare = function () {
-  if (this.depo[0].value > this.depo[1]) {
-    this.player1.hand = [...this.player1.hand, this.depo];
-    this.depo = [];
-    console.log('player1 wins round');
+  if (this.depo[0].value > this.depo[1].value) {
+    this.player1.hand = [...this.depo];
+    //this.player2.hand = [this.player2.hand];
+    //this.depo = [];
+    console.log("player1 wins round");
     console.log(this.player1.hand.length);
     // console.log(this.depo)
   } else if (this.depo[1].value > this.depo[0].value) {
-    this.player2.hand = [...this.player2.hand, this.depo];
-    this.depo = [];
-    console.log('player2 wins round');
+    this.player2.hand = [...this.depo];
+    //this.player1.hand = [this.player1.hand];
+    //this.depo = [];
+    console.log("player2 wins round");
     console.log(this.player2.hand.length);
     // console.log(this.depo)
   } else {
-    console.log("issa tie");
+    console.log("This means war!");
     war(this.player1, this.player2);
-    // this.depo = [...this.player1.hand.splice(0, 3)];
+    this.depo = [
+      ...this.player1.hand.splice(0, 3),
+      this.player2.hand.splice(0, 3),
+    ];
   } //this is the draw 3 for war and adding to the pot
 };
 
 function war(player1, player2) {
-  console.log("war is running");
+  //console.log("war is running");
   const player1WarCards = player1.hand.splice(0, 3);
   const player2WarCards = player2.hand.splice(0, 3);
 
   return compareWar(player1WarCards, player2WarCards);
-
-};
+}
 
 function compareWar(player1WarCards, player2WarCards) {
- // console.log(player1WarCards);
- // console.log(player1WarCards[2]);
- // console.log(player2WarCards[2]);
- let  player1WarCard = player1WarCards[2].value;
-let player2WarCard = player2WarCards[2].value;
+  // console.log(player1WarCards);
+  // console.log(player1WarCards[2]);
+  // console.log(player2WarCards[2]);
+  let player1WarCard = player1WarCards[2].value;
+  let player2WarCard = player2WarCards[2].value;
+  this.warDepo = [player1WarCard, player2WarCard];
 
-if (player1WarCard > player2WarCard) {
-  player1.hand = [...player1WarCards,...this.depo]
-  //console.log('Player1 Wins')
-
-} else if (player2WarCard > player1WarCards) {
-  player2.hand = [...this.player2WarCards,...this.depo]
-  //console.log('player2 wins')
+  if (player1WarCard > player2WarCard) {
+    player1.hand = [...this.warDepo];
+    console.log("Player1 Wins");
+  } else if (player2WarCard > player1WarCards) {
+    player2.hand = [...this.warDepo];
+    console.log("player2 wins");
+  }
 }
-};
-
-
-
-
-
